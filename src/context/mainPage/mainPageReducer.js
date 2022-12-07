90b6f63ab1b3669d356aeec86243a62bd41fde44
Max Solo <ma_solo@inbox.ru>
2022-12-07 22:39:52 +0300

    fix2

--- src/context/mainPage/mainPageReducer.js
+++ src/context/mainPage/mainPageReducer.js
@@ -33,13 +33,13 @@ export function mainPageReducer(state, {type, payload}) {
 		case ACTIVE_IMAGE_TOGGLE_NEXT:
 			return {
 				...state,
-				activeImage: state.activeImage === 5 ? state.activeImage = 1 : state.activeImage++
+				activeImage: state.activeImage === 4 ?  1 : state.activeImage + 1
 								 
 			}	
 		case ACTIVE_IMAGE_TOGGLE_PREV:
 			return {
 				...state,
-				activeImage: state.activeImage === 0 ? state.activeImage = 4 : state.activeImage--
+				activeImage: state.activeImage === 1 ?  4 : state.activeImage - 1
 								 
 			}	
 		case CURRENT_COUNTER:
